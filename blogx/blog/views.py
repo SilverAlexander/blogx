fd6651dada7d1f783daead03d480b8379555045a
SilverAlexander <59421429+SilverAlexander@users.noreply.github.com>
2020-04-21 17:08:58 +0100

    Updated blog views and templates

--- blogx/blog/views.py
+++ blogx/blog/views.py
@@ -1,13 +1,30 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from .models import Post
 
 
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/list.html'
+
+
 def post_list(request):
-    posts = Post.published.all()
+    object_list = Post.published.all()  # uses custom manager from models
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(
         request,
-        '/app/blogx/templates/blog/list.html',
-        {'posts': posts},
+        'blog/list.html',
+        {'page': page, 'posts': posts},
     )
 
 
@@ -22,6 +39,6 @@ def post_detail(request, year, month, day, post):
         )
     return render(
         request,
-        '/app/blogx/templates/blog/detail.html',
+        'blog/detail.html',
         {'post': post},
         )
