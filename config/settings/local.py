8aadf86bbc0677b79553e259b946a7c9bea290cf
SilverAlexander <59421429+SilverAlexander@users.noreply.github.com>
2020-04-21 21:34:43 +0100

    updated ignore

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,15 +1,18 @@
 from .base import *  # noqa
 from .base import env
 
+
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = True
+
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env(
     "DJANGO_SECRET_KEY",
     default="da6GQ43jps3ebP3wQsDWS1fvFjO1TXTXvJgirJqHByzZZvYad5ucuUPzZuboVE1j",
 )
+
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
