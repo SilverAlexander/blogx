76fd1f2a6c6a3b86885a8535f0c6d041eb978bc6
SilverAlexander <59421429+SilverAlexander@users.noreply.github.com>
2020-04-22 15:45:50 +0100

    Built email share form functionality

--- blogx/blog/urls.py
+++ blogx/blog/urls.py
@@ -11,6 +11,11 @@ urlpatterns = [
     path(
         '<int:year>/<int:month>/<int:day>/<slug:post>/',
         views.post_detail,
-        name="post_detail"
+        name="post_detail",
         ),
+    path(
+        '<int:post_id>/share/',
+        views.post_share,
+        name="post_share",
+    )
     ]
--- blogx/blog/views.py
+++ blogx/blog/views.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
@@ -46,16 +47,23 @@ def post_detail(request, year, month, day, post):
 
 
 def post_share(request, post_id):
+    sent = False
     post = get_object_or_404(Post, id=post_id, status='published',)
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'admin@example.com', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
     return render(
         request,
         'blog/share.html',
-        {'post': post, 'form': form},
+        {'post': post, 'form': form, 'sent': sent},
     )
